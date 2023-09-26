aba9bba753bd76c82b684e2c420bbea2b97f6558
Ankit kumar yadav <ankit.kumar.yadav@ibm.com>
2023-09-27 00:36:43 +0530

    change done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './components/Login'
+import Dashboard from './components/Dashboard';
+import LeftNavbar from './components/LeftNavbar';
+import RightNavbar from './components/RightNavbar';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate
+} from "react-router-dom";
+import ArtistPlayList from './components/ArtistPlayList';
+import { useSelector } from 'react-redux';
+import Search from './components/Search';
 
-function App() {
+
+const App = () => {
+  const globalState = useSelector((state)=>state.login.isLogin);
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:(globalState ? <><LeftNavbar /> <Dashboard /> <RightNavbar /> </>: <Navigate to="/login" />)
+    },
+    {
+      path:"/login",
+      element: (globalState ? <Navigate to="/" /> : <Login />)
+    },
+    {
+      path:"/artist",
+      element:(globalState ? <><LeftNavbar /> <ArtistPlayList /> <RightNavbar /> </>: <Navigate to="/login" />)
+    },
+    {
+      path: "/search",
+      element:(globalState ? <><LeftNavbar /> <Search /> <RightNavbar /> </>: <Navigate to="/login" />)
+    }
+  ])
+
+  const scssClass = "parent_box " + (globalState ? "three_column":"");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <section className={scssClass}>
+        <RouterProvider router={router} />
+      </section>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
